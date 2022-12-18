d319096daa584b9b329191946061985dbb651be5
JubayerDev <ajashes858@gmail.com>
2022-12-18 17:13:30 +0600

    single service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -26,7 +26,14 @@ async function run() {
             const cursor = serviceCollection.find({});
             const services = await cursor.toArray();
             res.send(services)
-       })
+        })
+        
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
     }
     finally {
         
